db625ac77c8d31177366e21e965ceab2288f397e
Pedrones00 <pedro.mugnol@gmail.com>
2025-10-05 08:17:26 -0300

    feat(exception): add exception handling for parameter type validation

--- src/main/java/com/bookstore/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bookstore/api/exception/GlobalExceptionHandler.java
@@ -6,12 +6,30 @@ import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.bookstore.api.exception.authorException.AuthorNotFoundException;
 import com.bookstore.api.exception.bookException.BookNotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Map<String, String>> handleTypeMismatchException(MethodArgumentTypeMismatchException ex){
+        
+        Map<String, String> error = new HashMap<>();
+
+        if (ex.getRequiredType() == Boolean.class) {
+            error.put("message", "The " + ex.getName() + " parameter must be of boolean type (true/false)");
+            error.put("status", "400");
+            return ResponseEntity.status(400).body(error);
+        }
+
+        error.put("message", "Error in " + ex.getPropertyName() + " parameter type.");
+        error.put("status", "400");
+
+        return ResponseEntity.status(400).body(error);
+    }
     
     @ExceptionHandler({AuthorNotFoundException.class})
     public ResponseEntity<Map<String, String>> handlerNotFound(AuthorNotFoundException ex){
